7df01cdefda4f690807686da617f7aa71e6e7f5d
Wonjong Park <wonjong2@gmail.com>
2022-05-17 13:16:42 -0700

    Implemented 'Get All Categories' and 'Get One Category'

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,32 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findAll({
+      include: [Product]
+    });
+    res.status(200).json(categoriesData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [Product]
+    });
+    res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
