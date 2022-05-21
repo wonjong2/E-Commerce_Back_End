075b30be99c1945ca8ec617f6483635c90476ec4
Wonjong Park <wonjong2@gmail.com>
2022-05-21 14:58:47 -0700

    changed POST handler in tag-routers.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+
 // The `/api/tags` endpoint
 router.get('/', async (req, res) => {
   // find all tags
@@ -14,6 +15,7 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
@@ -27,17 +29,33 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tag = await Tag.create(req.body);
-    res.status(200).json(tag);
+
+    // if there's product ids, we need to create pairings to bulk create in the ProductTag model
+    if(req.body.productIds?.length) {
+      const productTagIdArr = req.body.productIds.map((product_id) => {
+        return {
+          product_id,
+          tag_id: tag.id
+        }
+      }); 
+      const productTagIds = await ProductTag.bulkCreate(productTagIdArr);
+      res.status(200).json(productTagIds);
+    }
+    else {
+      res.status(200).json(tag);
+    }
   }
   catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
+
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
@@ -51,6 +69,7 @@ router.put('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
